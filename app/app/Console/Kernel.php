3352a7e69ed8477de10a5e071a7183cc22ab6e9f
YadiiCabeza96 <yadicabeza12.96@gmail.com>
2026-06-28 18:16:10 -0500

    cambios

---
+++ app/app/Console/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Console\Scheduling\Schedule;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+    ];
+
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        //
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        //
+    }
+}
